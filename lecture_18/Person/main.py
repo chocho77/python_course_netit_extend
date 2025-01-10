cc273a419f80b71105cf6b3f96bb25c24eca3799
chocho77 <chavmom@gmail.com>
2025-01-11 00:38:50 +0200

    new file:   lecture_18/Person/__pycache__/person.cpython-311.pyc 	new file:   lecture_18/Person/__pycache__/student.cpython-311.pyc 	new file:   lecture_18/Person/main.py 	modified:   lecture_18/Person/person.py 	modified:   lecture_18/Person/student.py 	modified:   lecture_18/Person/employee.py 	lecture_18/Person/__pycache__/employee.cpython-311.pyc

---
+++ lecture_18/Person/main.py
@@ -0,0 +1,13 @@
+from student import Student
+from employee import Employee
+
+def main():
+    student = Student("Chavdar", "Ventsislavov", "Momchilov", "chavmom@gmail.com")
+    print(student)
+    employee = Employee("Kalin", "Stanchev", "Ivanov", "kalin@gmail.com")
+    print(employee)
+    
+  
+
+if __name__ == '__main__':
+    main()
--- lecture_18/Person/person.py
+++ lecture_18/Person/person.py
@@ -1,16 +1,14 @@
 class Person:
     def __init__(self,
-                 first_name,
-                 middle_name,
-                 last_name,
-                 email_id):
-        self._first_name = first_name
-        self._middle_name = middle_name
-        self._last_name = last_name
-        self._email_id = email_id
+                 first_name="",
+                 middle_name="",
+                 last_name="",
+                 email_id=""):
+        self.first_name = first_name
+        self.middle_name = middle_name
+        self.last_name = last_name
+        self.email_id = email_id
     
-    @property
-    def first_name(self):
-        return self._first_name
+   
     
-    
+   
